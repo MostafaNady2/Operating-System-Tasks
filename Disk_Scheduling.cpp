ab9c3f9a7f4778ead0cf2c1f4e36b6c6035d7cf0
mostafa <Mustafa2nady@gmail.com>
2025-05-06 01:36:27 +0300

    Task 2

--- Disk_Scheduling.cpp
+++ Disk_Scheduling.cpp
@@ -44,7 +44,7 @@ public:
         cout << "-----------------------------------------------------" << endl;
         cout << "FCFS Algorithm" << endl;
         cout << "Total head movement: " << totalHeadMovements << " Cylinders" << endl;
-        cout<< "supposed each track take 0.1 ms , Seek Time : "<<totalHeadMovements*0.1<<" ms"<<endl;
+        cout << "supposed each track take 0.1 ms , Seek Time : " << totalHeadMovements * 0.1 << " ms" << endl;
         cout << "The Order of requests: ";
         for (int i = 0; i < queueSize; i++)
         {
@@ -54,9 +54,9 @@ public:
                 cout << " -> ";
             }
         }
-        cout << '\n';
+        cout << "\n-----------------------------------------------------" << endl;
     }
-    void SCAN()
+    void SCAN(int direction)
     {
         int head = initialHead;
         totalHeadMovements = 0;
@@ -64,37 +64,56 @@ public:
         vector<int> p;
         sort(temp.begin(), temp.end());
         int idx = lower_bound(temp.begin(), temp.end(), head) - temp.begin();
-        for (int i = idx; i < temp.size(); i++)
+        if (direction == 1) 
         {
-            int request = temp[i];
-            totalHeadMovements += abs(request - head);
-            head = request;
-            p.push_back(request);
+            for (int i = idx; i < temp.size(); i++)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
+            totalHeadMovements += abs((numOfTracks - 1) - head);
+            head = numOfTracks - 1;
+            for (int i = idx - 1; i >= 0; i--)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
         }
-        totalHeadMovements += abs((numOfTracks - 1) - head);
-        head = numOfTracks - 1;
-        p.push_back(numOfTracks - 1);
-        for (int i = idx - 1; i >= 0; i--)
+        else 
         {
-            int request = temp[i];
-            totalHeadMovements += abs(head - request);
-            head = request;
-            p.push_back(request);
+            for (int i = idx - 1; i >= 0; i--)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
+            totalHeadMovements += head;
+            head = 0;
+            for (int i = idx; i < temp.size(); i++)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
         }
         cout << "-----------------------------------------------------" << endl;
         cout << "SCAN Algorithm" << endl;
         cout << "Total head movement: " << totalHeadMovements << " Cylinders" << endl;
-        cout<< "supposed each track take 0.1 ms , Seek Time : "<<totalHeadMovements*0.1<<" ms"<<endl;
+        cout << "Suppose each track takes 0.1 ms, Seek Time: " << totalHeadMovements * 0.1 << " ms" << endl;
         cout << "The Order of requests: ";
         for (int i = 0; i < p.size(); i++)
         {
             cout << p[i];
             if (i != p.size() - 1)
+            {
                 cout << " -> ";
+            }
         }
-        cout << '\n';
+        cout << "\n-----------------------------------------------------" << endl;
     }
-    void CSCAN()
+    void CSCAN(int direction)
     {
         int head = initialHead;
         totalHeadMovements = 0;
@@ -102,38 +121,59 @@ public:
         vector<int> p;
         sort(temp.begin(), temp.end());
         int idx = lower_bound(temp.begin(), temp.end(), head) - temp.begin();
-        for (int i = idx; i < temp.size(); i++)
+        if (direction == 1)
         {
-            int request = temp[i];
-            totalHeadMovements += abs(request - head);
-            head = request;
-            p.push_back(request);
+            for (int i = idx; i < temp.size(); i++)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
+            totalHeadMovements += abs((numOfTracks - 1) - head);
+            head = 0;
+            totalHeadMovements += head;
+            for (int i = 0; i < idx; i++)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
         }
-        totalHeadMovements += abs((numOfTracks - 1) - head);
-        head = numOfTracks - 1;
-        p.push_back(numOfTracks - 1);
-        totalHeadMovements += abs(0 - head);
-        head = 0;
-        p.push_back(0);
-        for (int i = 0; i < idx; i++)
+        else
         {
-            int request = temp[i];
-            totalHeadMovements += abs(head - request);
-            head = request;
-            p.push_back(request);
+            for (int i = idx - 1; i >= 0; i--)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
+
+            totalHeadMovements += head;
+            head = numOfTracks - 1;
+            totalHeadMovements += head;
+
+            for (int i = temp.size() - 1; i >= idx; i--)
+            {
+                totalHeadMovements += abs(temp[i] - head);
+                head = temp[i];
+                p.push_back(head);
+            }
         }
+
         cout << "-----------------------------------------------------" << endl;
         cout << "CSCAN Algorithm" << endl;
         cout << "Total head movement: " << totalHeadMovements << " Cylinders" << endl;
-        cout<< "supposed each track take 0.1 ms , Seek Time : "<<totalHeadMovements*0.1<<" ms"<<endl;
+        cout << "Suppose each track takes 0.1 ms, Seek Time: " << totalHeadMovements * 0.1 << " ms" << endl;
         cout << "The Order of requests: ";
         for (int i = 0; i < p.size(); i++)
         {
             cout << p[i];
             if (i != p.size() - 1)
+            {
                 cout << " -> ";
+            }
         }
-        cout << '\n';
+        cout << "\n-----------------------------------------------------" << endl;
     }
     ~Scheduler()
     {
@@ -155,9 +195,49 @@ int main()
     cin >> initialHead;
     Scheduler s(queueSize, initialHead, numOfTracks);
     s.fillRequests();
-    s.FCFS();
-    s.SCAN();
-    s.CSCAN();
+    while (true)
+    {
+        cout << "Choose the scheduling algorithm : \n";
+        cout << "1 : FCFS \n2 : SCAN \n3 : CSCAN\n";
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            s.FCFS();
+            break;
+        case 2:
+            cout << "Enter the direction : \n1 : Clockwise\n2 : Anticlockwise\n";
+            int direction;
+            cin >> direction;
+            if (direction != 1 && direction != 2)
+            {
+                cout << "Invalid direction" << endl;
+                break;
+            }
+            s.SCAN(direction);
+            break;
+        case 3:
+            cout << "Enter the direction : \n1 : Clockwise\n2 : Anticlockwise\n";
+            cin >> direction;
+            if (direction != 1 && direction != 2)
+            {
+                cout << "Invalid direction" << endl;
+                break;
+            }
+            s.CSCAN(direction);
+            break;
+        default:
+            break;
+        }
+        cout << "Do you want to continue? (y/n) : ";
+        char ch;
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+        {
+            break;
+        }
+    }
 
     return 0;
 }
